34252277f5821566c37b69bd20c08b575fcd4f6a
Tim Fechner <tim.fechner@hs-hannover.de>
2016-03-18 15:17:58 +0100

    Improve serverdoc command
    now deletes entities which never touched in a single run (indicies that
    this entity is obviously deleted)

--- salt_observer/management/commands/serverdoc.py
+++ salt_observer/management/commands/serverdoc.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
 
 from salt_observer.cherry import SaltCherrypyApi
 from salt_observer.models import Minion, Network, NetworkInterface
 
 from getpass import getpass
-from datetime import datetime
 import netaddr
 import json
 
@@ -12,52 +12,67 @@ import json
 class Command(BaseCommand):
     help = 'Fetch and save new data from all servers'
 
-    def handle(self, *args, **options):
-        m = SaltCherrypyApi(input('Username: '), getpass())
+    def _update_data(self, grains):
+        ''' Update everything we can update out of the grains '''
+        touched = {'minions': [], 'networks': [], 'interfaces': []}
+        for minion_fqdn, minion_grains in grains.items():
+            minion = self._update_minion(minion_fqdn, minion_grains)
+            touched['minions'].append(minion)
 
-        all_grains = m.get_server_information()
+            if 'so_interfaces' in minion_grains.keys():
+                self._update_connections(minion_grains['so_interfaces'], minion, touched)
+        return touched
 
-        for minion_fqdn, minion_grains in all_grains.items():
-            minion = Minion.objects.filter(fqdn=minion_fqdn).first()
-            self.stdout.write('{:>50} '.format(minion_fqdn), ending='')
+    def _update_minion(self, fqdn, grains):
+        ''' Update minions related to grains we got '''
+        minion = Minion.objects.filter(fqdn=fqdn).first()
+        if minion:
+            minion.grains = json.dumps(grains)
+        else:
+            minion = Minion(fqdn=fqdn, grains=json.dumps(grains), timestamp=timezone.now())
+        minion.save()
+        return minion
 
-            if minion:
-                self.stdout.write(self.style.SUCCESS('Update'))
-                minion.grains = json.dumps(minion_grains)
-            else:
-                self.stdout.write(self.style.WARNING('Create'))
-                minion = Minion(
-                    fqdn=minion_fqdn,
-                    grains=json.dumps(minion_grains),
-                    timestamp=datetime.now()
-                )
-            minion.save()
-
-            if 'so_interfaces' not in minion_grains.keys():
+    def _update_connections(self, interfaces, minion, touched):
+        ''' Update networks and network interfaces '''
+        for if_name, if_data in interfaces.items():
+            if if_name in ['lo', 'lo0']:
                 continue
 
-            for if_name, if_data in minion_grains['so_interfaces'].items():
-                if if_name in ['lo', 'lo0']:
-                    continue
+            ipv4_network = str(netaddr.IPNetwork('{}/{}'.format(if_data['ipv4']['address'], if_data['ipv4']['netmask'])).network)
+            network = self._update_network(ipv4_network, if_data['ipv4']['netmask'])
+            touched['networks'].append(network)
+            interface = self._update_network_interface(network, minion, if_data['mac_address'], if_name)
+            touched['interfaces'].append(interface)
 
-                ipv4_network = str(netaddr.IPNetwork('{}/{}'.format(if_data['ipv4']['address'], if_data['ipv4']['netmask'])).network)
+    def _update_network(self, ipv4, mask):
+        ''' Update the Network objects '''
+        network = Network.objects.filter(ipv4=ipv4).first()
+        if not network:
+            network = Network(ipv4=ipv4, mask=mask)
+            network.save()
+        return network
 
-                self.stdout.write('{:>50} '.format(ipv4_network), ending='')
-                network = Network.objects.filter(ipv4=ipv4_network).first()
-                if not network:
-                    self.stdout.write(self.style.WARNING('Create'))
-                    network = Network(ipv4=ipv4_network, mask=if_data['ipv4']['netmask'])
-                else:
-                    self.stdout.write(self.style.SUCCESS('Update'))
-                network.save()
+    def _update_network_interface(self, network, minion, mac_address, if_name):
+        ''' Update all network interfaces '''
+        ni = NetworkInterface.objects.filter(network=network, minion=minion, mac_address=mac_address).first()
+        if not ni:
+            ni = NetworkInterface(network=network, minion=minion, mac_address=mac_address, name=if_name).save()
+        return ni
 
-                self.stdout.write('{:>50} '.format(if_data['mac_address']), ending='')
-                ni = NetworkInterface.objects.filter(network=network, minion=minion, mac_address=if_data['mac_address']).first()
-                if not ni:
-                    self.stdout.write(self.style.WARNING('Create'))
-                    ni = NetworkInterface(network=network, minion=minion, mac_address=if_data['mac_address'], name=if_name)
-                else:
-                    self.stdout.write(self.style.SUCCESS('Update'))
-                ni.save()
+    def _cleanup(self, touched):
+        for minion in Minion.objects.all():
+            if minion not in touched['minions']:
+                minion.delete()
+        for network in Network.objects.all():
+            if network not in touched['networks']:
+                network.delete()
+        for interface in NetworkInterface.objects.all():
+            if interface not in touched['interfaces']:
+                interface.delete()
 
+    def handle(self, *args, **options):
+        m = SaltCherrypyApi(input('Username: '), getpass())
+        touched_elements = self._update_data(m.get_server_information())
+        self._cleanup(touched_elements)
         m.logout()
