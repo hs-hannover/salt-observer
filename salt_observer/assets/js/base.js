bc3e15ae3f4767de26fb86f3095107af8944b427
Tim Fechner <tim.fechner@hs-hannover.de>
2016-07-18 17:28:37 +0200

    Move sortable javascriot to base.js

--- salt_observer/assets/js/base.js
+++ salt_observer/assets/js/base.js
@@ -1,3 +1,19 @@
-$('.title-wrapper').click(function() {
-    window.location = $(this).data('root');
+$(function() {
+    $('.title-wrapper').click(function() {
+        window.location = $(this).data('root');
+    });
+
+    // some little helpers
+    $.tablesorter.themes.bootstrap = {
+        header: 'bootstrap-header',
+        iconSortNone: 'fa fa-sort',
+        iconSortAsc: 'fa fa-sort-asc',
+        iconSortDesc: 'fa fa-sort-desc',
+    };
+    $('table.sortable').tablesorter({
+        theme: 'bootstrap',
+        headerTemplate: '{content} {icon}',
+        sortList: [[0,0]],
+        widgets: ['uitheme'],
+    });
 });
